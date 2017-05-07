5f423d9cbf326f9fbc795a238d8a018f8815b2e4
Samuel Spencer <theodore.therone@gmail.com>
2017-05-07 23:42:27 +0000

    uncomment exception code

--- django_limits/exceptions.py
+++ django_limits/exceptions.py
@@ -8,9 +8,9 @@ class LimitExceeded(exceptions.PermissionDenied):
         self.template = template
         super(LimitExceeded, self).__init__(*args, **kwargs)
 
-    # def __str__(self):
-    #     return "LimitExceeded - Your plan only allows for number} name} - {message}".format(
-    #         # number=self.details['limit'],
-    #         # message=self.details['message'],
-    #         name=self.model._meta.verbose_name_plural
-    #     )
+    def __str__(self):
+        return "LimitExceeded - Your plan only allows for {number} {name} - {message}".format(
+            number=self.details['limit'],
+            message=self.details['message'],
+            name=self.model._meta.verbose_name_plural
+        )
